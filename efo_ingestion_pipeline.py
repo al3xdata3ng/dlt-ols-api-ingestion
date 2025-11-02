0e010784bf174f5e474ad95893d305a93be379b1
Alexandros Vitsas <al3xdata3ng@gmail.com>
2025-11-02 16:19:55 +0200

    config: added contents of .log to gitignore

--- efo_ingestion_pipeline.py
+++ efo_ingestion_pipeline.py
@@ -7,6 +7,8 @@ from efo_source import efo_source
 
 efo_pipeline_config = EfoPipelineConfig()
 
+print(f"./.logs/{efo_pipeline_config.PIPELINE_NAME}.log")
+
 # --- 1️⃣ Configure logging ---
 logger = logging.getLogger(efo_pipeline_config.PIPELINE_NAME)
 logger.setLevel(logging.INFO)
@@ -18,7 +20,7 @@ console_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s
 console_handler.setFormatter(console_formatter)
 
 # File handler
-file_handler = logging.FileHandler(f"./.logs/{efo_pipeline_config.PIPELINE_NAME}.log")
+file_handler = logging.FileHandler(f"./.log/{efo_pipeline_config.PIPELINE_NAME}.log")
 file_handler.setLevel(logging.INFO)
 file_formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 file_handler.setFormatter(file_formatter)
